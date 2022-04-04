0a57c5aa87e7a847f6794caaa6dceb17a59dbda7
aluno <aluno@10.107.71.24>
2022-04-04 11:27:22 -0300

    lol

---
+++ OrientaçãoObjetos/src/Pessoas.java
@@ -0,0 +1,15 @@
+
+public class Pessoas {
+	
+	String nome;
+	int nume_figu;
+	
+	void receber(int nume_figu) {
+		this.nume_figu += nume_figu;
+		
+		
+	}
+	void dar( ) {
+		
+	}
+}
---
+++ OrientaçãoObjetos/src/main.java
@@ -0,0 +1,18 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Pessoas pessoa1 = new Pessoas();
+		
+		pessoa1.nome = "Margus";
+		pessoa1.nume_figu = 43;
+		
+		pessoa1.receber(78);
+		
+		System.out.println(pessoa1.nome);
+		System.out.println(pessoa1.nume_figu);
+		
+	}
+
+}
